97177ce98655df4e63a975d9dd4697d72e6b75b9
Zahar <zaharbypynskiuzk@gmail.com>
2026-03-11 10:41:47 +0100

    add endpoints/simple CRUD/id for task/Pydantic

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Hello"}
+
+tasks = []
+
+@app.get("/tasks")
+def get_tasks():
+    return tasks
+
+@app.post("/tasks")
+def create_task(task: dict):
+    tasks.append(task)
+    return task
