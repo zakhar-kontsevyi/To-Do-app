441a4f51a04e33db122d640bdcfbb0ebd2354a93
Zahar <zaharbypynskiuzk@gmail.com>
2026-03-11 10:45:09 +0100

    add endpoints/simple CRUD/id for task/Pydantic

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Task(BaseModel):
+    title: str
+    description: str | None = None
+
+
 
 app = FastAPI()
 
@@ -6,13 +13,35 @@ app = FastAPI()
 def root():
     return {"message": "Hello"}
 
-tasks = []
+
 
 @app.get("/tasks")
 def get_tasks():
     return tasks
 
+tasks = []
+counter = 1
+
+@app.post("/tasks")
+def create_task(task: Task):
+    global counter
+
+    new_task = {
+        "id": counter,
+        "title": task.title,
+        "description": task.description
+    }
+
+    tasks.append(new_task)
+    counter += 1
+    return new_task
+
+
 @app.post("/tasks")
 def create_task(task: dict):
     tasks.append(task)
     return task
+
+@app.get("/tasks/{task_id}")
+@app.put("/tasks/{task_id}")
+@app.delete("/tasks/{task_id}")
